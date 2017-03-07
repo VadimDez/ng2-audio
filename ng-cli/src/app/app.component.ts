d9a6bd9e01378e69569c31cd7ef48326347200fa
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-03-07 13:23:54 +0100

    audio

--- ng-cli/src/app/app.component.ts
+++ ng-cli/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  audioSrc = 'https://archive.org/download/testmp3testfile/mpthreetest.mp3';
+  @ViewChild('audio') audio: ElementRef;
+
+  constructor() {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', this.audioSrc, true);
+    xhr.responseType = 'blob';
+    xhr.onload = (evt) => {
+      const blob = new Blob([xhr.response], {type: 'audio/mpeg'});
+      let objectUrl = URL.createObjectURL(blob);
+      this.audio.nativeElement.src = objectUrl;
+      // Release resource when it's loaded
+      this.audio.nativeElement.onload = function(evt) {
+        URL.createObjectURL(objectUrl);
+      };
+      this.audio.nativeElement.play();
+    };
+
+    xhr.send();
+  }
 }
